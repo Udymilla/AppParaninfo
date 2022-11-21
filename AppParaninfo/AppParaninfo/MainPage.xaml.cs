28428bd911bbb2a0576225531683ad532eccacd3
Usuario <Usuario@CPD03-M05>
2022-11-21 13:37:59 -0300

    Adicionar arquivos de projeto.

---
+++ AppParaninfo/AppParaninfo/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppParaninfo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SIM());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var result = DisplayAlert("OPÇÃO INDISPOVIVEL", " Somos obrigados a fazer suas provas, então você tambem não tem escolha <3 ", "ok?");
+        }
+    }
+}
---
+++ AppParaninfo/AppParaninfo/SIM.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppParaninfo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SIM : ContentPage
+    {
+        public SIM()
+        {
+            InitializeComponent();
+        }
+    }
+}
